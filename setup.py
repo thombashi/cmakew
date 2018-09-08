21f701456da3cf8895719ef00164224df5c65304
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-09-08 17:45:32 +0900

    Add authorship information

--- setup.py
+++ setup.py
@@ -6,39 +6,47 @@
 
 from __future__ import unicode_literals
 
+import io
 import os.path
 import sys
 
 import setuptools
 
 
+MODULE_NAME = "cmakew"
+REPOSITORY_URL = "https://github.com/thombashi/{:s}".format(MODULE_NAME)
 REQUIREMENT_DIR = "requirements"
+ENCODING = "utf8"
+
+pkg_info = {}
 
 
-with open("README.rst") as fp:
+with open(os.path.join(MODULE_NAME, "__version__.py")) as f:
+    exec(f.read(), pkg_info)
+
+with io.open("README.rst", encoding=ENCODING) as fp:
     long_description = fp.read()
 
-with open(os.path.join("docs", "pages", "introduction", "summary.txt")) as f:
-    summary = f.read()
+with io.open(os.path.join("docs", "pages", "introduction", "summary.txt"), encoding=ENCODING) as f:
+    summary = f.read().strip()
 
 with open(os.path.join(REQUIREMENT_DIR, "requirements.txt")) as f:
     install_requires = [line.strip() for line in f if line.strip()]
 
-MODULE_NAME = "cmakew"
 needs_pytest = set(["pytest", "test", "ptr"]).intersection(sys.argv)
 pytest_runner = ["pytest-runner"] if needs_pytest else []
 
 setuptools.setup(
     name=MODULE_NAME,
-    version="0.0.6",
+    version=pkg_info["__version__"],
     url="https://github.com/thombashi/{:s}".format(MODULE_NAME),
 
-    author="Tsuyoshi Hombashi",
-    author_email="tsuyoshi.hombashi@gmail.com",
+    author=pkg_info["__author__"],
+    author_email=pkg_info["__email__"],
     description=summary,
     include_package_data=True,
     keywords=["cmake", "build", "Visual Studio"],
-    license="MIT License",
+    license=pkg_info["__license__"],
     long_description=long_description,
     packages=setuptools.find_packages(exclude=['test*']),
     project_urls={
